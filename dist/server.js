1cb2530414e9ac0d8947d91e28a2fcf918becfbe
Izaan <mdizaan67@gmail.com>
2022-08-02 18:08:13 +0530

    updte build -- dist

--- dist/server.js
+++ dist/server.js
@@ -15,8 +15,8 @@ app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
 app.use('/admin', adminRoute_1.default);
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT ? process.env.PORT : 3100;
 app.listen(PORT, () => {
     (0, connectDB_1.default)(process.env.MONGODB_PASSWORD);
-    console.log('[CONNECTED_TO_SERVER]::http://localhost:3100');
+    console.log(`[CONNECTED_TO_SERVER] -[PORT::${PORT}]`);
 });
